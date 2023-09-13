39fc3dfc0011523b59ea2b7654d6865549b8d0c7
ryohei108@icloud.com <ryohei108@icloud.com>
2023-09-14 01:47:45 +0900

    bug fix

--- front/app/gikyoku-app/pages/api/search.ts
+++ front/app/gikyoku-app/pages/api/search.ts
@@ -59,9 +59,6 @@ export default async function handler(
         title: {
           contains: keyword as string,
         },
-        synopsis: {
-          contains: keyword as string,
-        },
 
         ...(ids.length > 0
           ? {
