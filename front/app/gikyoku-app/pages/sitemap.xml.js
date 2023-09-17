83dad6393acb9714a219247eecf7a443593be46b
ICHINOMIYA <ichiryo108@gmail.com>
2023-09-17 18:12:24 +0900

    sitemap

--- front/app/gikyoku-app/pages/sitemap.xml.js
+++ front/app/gikyoku-app/pages/sitemap.xml.js
@@ -1,24 +1,71 @@
-const EXTERNAL_DATA_URL = "https://gikyokutosyokan.com/posts";
-
-function generateSiteMap(posts) {
+const EXTERNAL_DATA_URL = "https://gikyokutosyokan.com/";
+const prisma = new PrismaClient();
+function generateSiteMap(posts, authors, categories) {
   return `<?xml version="1.0" encoding="UTF-8"?>
    <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
      <!--We manually set the two URLs we know already-->
-     <url>
-       <loc>https://jsonplaceholder.typicode.com</loc>
-     </url>
-     <url>
-       <loc>https://jsonplaceholder.typicode.com/guide</loc>
-     </url>
      ${posts
        .map(({ id }) => {
          return `
        <url>
-           <loc>${`${EXTERNAL_DATA_URL}/${id}`}</loc>
+           <loc>${`${EXTERNAL_DATA_URL}/posts/${id}`}</loc>
        </url>
      `;
        })
        .join("")}
+
+     <!-- Add /authors/[id], /categories/[id], /authors, /categories, and other custom routes here -->
+     <url>
+       <loc>${`${EXTERNAL_DATA_URL}/authors`}</loc>
+     </url>
+     <url>
+       <loc>${`${EXTERNAL_DATA_URL}/categories`}</loc>
+     </url>
+     ${authors
+       .map(({ id }) => {
+         return `
+       <url>
+           <loc>${`${EXTERNAL_DATA_URL}/authors/${id}`}</loc>
+       </url>
+     `;
+       })
+       .join("")}
+     ${categories
+       .map(({ id }) => {
+         return `
+       <url>
+           <loc>${`${EXTERNAL_DATA_URL}/categories/${id}`}</loc>
+       </url>
+     `;
+       })
+       .join("")}
+     <url>
+       <loc>${`${EXTERNAL_DATA_URL}/support/about`}</loc>
+     </url>
+     <url>
+       <loc>${`${EXTERNAL_DATA_URL}/support/contact`}</loc>
+     </url>
+     <url>
+       <loc>${`${EXTERNAL_DATA_URL}/support/copyright`}</loc>
+     </url>
+     <url>
+       <loc>${`${EXTERNAL_DATA_URL}/support/aboutus`}</loc>
+     </url>
+     <url>
+       <loc>${`${EXTERNAL_DATA_URL}/support/posting-request`}</loc>
+     </url>
+     <url>
+       <loc>${`${EXTERNAL_DATA_URL}/support/privacy-policy`}</loc>
+     </url>
+     <url>
+       <loc>${`${EXTERNAL_DATA_URL}/support/press-release`}</loc>
+     </url>
+     <url>
+       <loc>${`${EXTERNAL_DATA_URL}/support/tos`}</loc>
+     </url>
+     <url>
+       <loc>${`${EXTERNAL_DATA_URL}/support/voluntary`}</loc>
+     </url>
    </urlset>
  `;
 }
@@ -28,17 +75,18 @@ function SiteMap() {
 }
 
 export async function getServerSideProps({ res }) {
-  // We make an API call to gather the URLs for our site
-  const request = await fetch(EXTERNAL_DATA_URL);
-  const posts = await request.json();
+  // We make API calls to gather the data for your site
+  const posts = await prisma.post.findMany();
+  const authors = await prisma.author.findMany();
+  const categories = await prisma.category.findMany();
 
-  // We generate the XML sitemap with the posts data
-  const sitemap = generateSiteMap(posts);
-
+  // We generate the XML sitemap with the data
+  const sitemap = generateSiteMap(posts, authors, categories);
+  res.statusCode = 200;
+  res.setHeader("Cache-Control", "s-maxage=86400, stale-while-revalidate"); // 24時間のキャッシュ
   res.setHeader("Content-Type", "text/xml");
   // we send the XML to the browser
-  res.write(sitemap);
-  res.end();
+  res.end(sitemap);
 
   return {
     props: {},
