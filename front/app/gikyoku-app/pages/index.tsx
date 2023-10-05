8117d5013ac3bb1d9b9ecf2fa2127f7c11b27442
ryohei108@icloud.com <ryohei108@icloud.com>
2023-10-05 20:21:28 +0900

    change ref

--- front/app/gikyoku-app/pages/index.tsx
+++ front/app/gikyoku-app/pages/index.tsx
@@ -39,7 +39,8 @@ export default function Home({ news, authors, posts, categories }: any) {
         />
         <TopImage buttonClick={handleScrollToRegistrationForm} />
         <NewsList news={news} />
-        <div className="lg:flex relative  box-border" id="registration-form">
+        <div className="lg:flex relative  box-border" id="registration-form"  ref={searchFormRef}>
+        <div></div>
           <SearchForm
             setData={setData}
             page={page}
@@ -51,7 +52,7 @@ export default function Home({ news, authors, posts, categories }: any) {
           <div className="lg:w-2/3 flex flex-col gap-3 m-1 md:m-5">
             {data ? (
               <>
-                <div ref={searchFormRef}></div>
+               
                 <DropBox
                   sort_by={sort_by}
                   setSortIndex={setSortIndex}
