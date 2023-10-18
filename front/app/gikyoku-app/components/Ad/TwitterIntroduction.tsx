52e63b60bef771cd78aee009242fc7ce0e36c077
ICHINOMIYA <ichiryo108@gmail.com>
2023-10-18 23:16:13 +0900

    admin

--- front/app/gikyoku-app/components/Ad/TwitterIntroduction.tsx
+++ front/app/gikyoku-app/components/Ad/TwitterIntroduction.tsx
@@ -10,13 +10,18 @@ const TwitterIntroduction = ({ post }: any) => {
         』のあらすじと感想を公開しました！　読んだ！上演した！上演する！などご気軽にコメントください！
       </p>
       <p>{post.synopsis}</p>
+
+      <p>
+        男:{post.man} 女:{post.woman} 総人数:{post.totalNumber} 上演時間:約
+        {post.playtime}分
+      </p>
       <Link href={`https://gikyokutosyokan.com/posts/${post.id}`}>
         https://gikyokutosyokan.com/posts/{post.id}
       </Link>
+      <p>{post.content}</p>
       <p>
         #{post.author.name} #{post.title} #{post.author.group} #戯デジ
-        #戯曲デジタルアーカイブ #ヒューマンドラマ #コメディ #演劇 #戯曲
-        #銀河鉄道の夜 #戯曲図書館 #戯曲 #脚本{" "}
+        #戯曲デジタルアーカイブ #演劇 #戯曲 #戯曲図書館 #戯曲 #脚本{" "}
         {post.categories.map((category: any) => `#${category}`).join(" ")}
       </p>
     </>
